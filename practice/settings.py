a19cddc4b32d6265840999e45136e5fd9b1ea3ae
Christian Mae Mendez <Christianmae041997@gmail.com>
2020-03-24 19:28:24 +0800

    database

--- practice/settings.py
+++ practice/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'prac',
-        'USER': 'admin',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '5432', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
